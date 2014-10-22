dcb145ad0c8bed354760f02b2c0e98c94b39782a
kazukimatsu <dango.umai@outlook.jp>
2014-10-22 23:23:44 +0900

    変数から数を抽出するメソッドを追加

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+static int totalNum=0;
+
 add(){}//←頑張って
 sub(){}//←頑張って
 mult(){}//←頑張って
 divi(){}//←頑張って
+
+int analysis(char[]);
+void countNumber();
 
 int main(){
 	char formula[100];
@@ -41,4 +46,35 @@ int main(){
 	}
 	ans = 0;
 	printf("formula=%s,ans=%d\n",formula, ans);
+	return 0;
+}
+
+int analysis(char a[]) /* 入力された式から数だけを抜き出す */
+{
+	for(int i=0;i<100;i++){
+			case '+':
+				continue;
+			case '-':
+				continue;
+			case '*':
+				continue;
+			case '/':
+				continue;
+			case '(':
+				continue;
+			case ')':
+				continu;;
+			case ' ':
+				//空白を無視
+				continue;
+			default:
+				countNumber();//ここで引数として処理する数を数える。
+				/*return a[i];*/
+				continue;
+	}
+	
+	void countNumber(){
+	totalNum++;
+	return;
+	}
 }
