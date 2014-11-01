138e7b2121bc3b0362999afbf8979614b4cb25d0
sinshutu <c0114292a2@edu.teu.ac.jp>
2014-11-01 13:17:16 +0900

    ちょっと改良

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
+/*注意事項
+//dはデバック処理用
+**この辺のことはそのうちreadmeに移ります。
+*/
+
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
+//初期化--------------------------------------------------------
 	char siki[100];
 	char num_tmp[100];
+	char *not_num_tmp;
 	double nums[100];
 	int i=0, nums_count=0, head_ptr=0, j=0, fptr=0;
 
@@ -12,37 +19,36 @@ int main()
 		siki[i] = '\0';
 		nums[i] = ' ';
 	}
+//ここまで初期化----------------------------------------------------
 
-	i = 0;
 	while((siki[fptr++] = getchar()) != '\n'){
 		printf("%d\n", siki[fptr-1]);	//d
 		printf("siki~:%s\n", siki);	//d
 		if(siki[fptr-1]==' ' || siki[fptr]=='\n'){
 			siki[fptr-1] = '\0';
-			strncpy(num_tmp, siki + head_ptr, fptr-1);
+			strncpy(num_tmp, siki + head_ptr, fptr-1);//数式から数字の場所だけ指定している。
 
 			printf("num_tmp:%s\n", num_tmp);//d
-
+			num_tmp[fptr - head_ptr] = '\0';
 			head_ptr = fptr;
 			// int a = atoi(num_tmp);//d
 			// nums[0] = (double)a;//d
-			nums[nums_count++] = (double)(atoi(num_tmp));//double	にできない
-			
-			// nums_count++;
+			nums[nums_count++] = strtod(&num_tmp,&not_num_tmp);//strtodは文字列を小数に変換する（うまくいってない）
+			printf("===>%1.1f\n", strtod(&num_tmp,&not_num_tmp));
 			// printf("a:%1.1f\n", (double)a + 1.0);//d
 			// printf("num_tmp:%d\n", atoi(num_tmp));//d
 			// printf("nums[]:%1.1f\n", nums[0]);//d	
 			siki[fptr-1] = ' ';
 		}
 	}
-	if(siki[i-1] == ' '){
+	if(siki[i-1] != ' '){
 		printf("siki:%s\n", siki);
 		while(nums[j] != ' '){
-		printf("%1.1f\n", nums[j]);
+		printf("[%d] %1.1f\n", j, nums[j]);
 		j++;
 		}	
 	}
-	printf("footer:%d\n", i);
+	printf("footer:%d\n", fptr);
 
 	return 0;
 }
